26258da45f1f1b0c1875550d09ce891f82dca2c8
StafaAlasady <Mustafa.alasady@outlook.com>
2024-02-18 03:06:45 +0100

    A start to my webbshop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Navbar } from './Components/TopNavbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Product } from './Pages/Product';
+import { MarketPlaceCategory } from './Pages/MarketPlaceCategory';
+import { Cart } from './Pages/Cart';
+import { LoginSignup } from './Pages/LoginSignup';
+import { Home } from './Pages/Home';
+import { Explore } from './Pages/Explore';
+import { ContactUs } from './Pages/Contact-us';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/MarketPlace' element={<MarketPlaceCategory category="Artworks"/>}/>
+        <Route path="/Explore" element={<Explore/>}/>
+        <Route path="/Contact-us" element={<ContactUs/>}/>
+        <Route path="/Product" element={<Product/>}>
+
+          <Route path=':productId' element={<Product/>}/>
+
+        </Route>
+        <Route path='/Cart' element={<Cart/>}/>
+        <Route path='/Login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/TopNavbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../Assets/No-back-logo.svg'
+import ShoppingCart2LineIcon from 'remixicon-react/ShoppingCart2LineIcon';
+import { Link } from 'react-router-dom';
+export const Navbar = () => {
+
+    const [menu,setMenu] = useState("shop");
+
+  return (
+<div className='navbar'>
+  <div className='nav-logo'>
+    <img src={logo} alt="" className='navbar-logo'/>
+    <p>AI.ART</p>
+  </div>
+  <ul className='nav-menu'>
+    <li onClick={()=>{setMenu("Home")}}><Link style={{ textDecoration:'none'}} to='/'>Home </Link>{menu==="Home"?<hr/>:<></>} </li>
+    <li onClick={()=>{setMenu("MarketPlace")}}><Link style={{ textDecoration: 'none'}} to='/MarketPlace'>MarketPlace </Link>{menu==="MarketPlace"?<hr/>:<></>} </li>
+    <li onClick={()=>{setMenu("Explore")}}><Link style={{ textDecoration: 'none'}} to='/Explore'>Explore </Link>{menu==="Explore"?<hr/>:<></>} </li>
+    <li onClick={()=>{setMenu("Contact Us")}}><Link style={{ textDecoration: 'none'}} to='/Contact-us'>Contact Us </Link>{menu==="Contact Us"?<hr/>:<></>} </li>
+  </ul>
+  <div className='nav-login-cart'>
+    <Link to='/Login'><button>Login</button></Link>
+      {/* The default color is the current text color (currentColor) */}
+      <Link to='/Cart'> <ShoppingCart2LineIcon className='cart-icon' /></Link>
+      <div className='nav-cart-count'>0</div>
+  </div>
+</div>
+  )
+}
