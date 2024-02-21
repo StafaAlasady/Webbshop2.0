e2717d2efe32b812aa6c8004fd1730965c1fbb5b
StafaAlasady <Mustafa.alasady@outlook.com>
2024-02-22 00:33:28 +0100

    little more on home page

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,24 @@
+import city from '../Assets/city.jpg'
+import blackwhitecity from '../Assets/black-whitecity.jpg'
+
+
+// Define your image data
+let data_product = [
+  {
+    id: 1,
+    name: 'Big colored city',
+    image: city,
+    new_price:700.00,
+    old_price:1200.00,
+  },
+  {
+    id: 2,
+    name: 'A black and white city',
+    image: blackwhitecity,
+    new_price:800.00,
+    old_price:1200.00,
+  },
+  // Add more image objects as needed
+];
+
+export default data_product;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Hero } from '../Components/Hero/Hero'
+import { Popular } from '../Components/Popular/popular'
+
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+      <Popular/>
+    </div>
   )
 }
