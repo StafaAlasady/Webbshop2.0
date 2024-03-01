8f25cb332f3700a69784634637160cd0e3fae1f0
StafaAlasady <Mustafa.alasady@outlook.com>
2024-03-01 16:38:19 +0100

    added js file containing all images

---
+++ src/Components/Assets/all_products.js
@@ -0,0 +1,248 @@
+import img_1 from '../Assets/img_1city.jpg'
+import img_2 from '../Assets/img_2black-whitecity.jpg'
+import img_3 from '../Assets/img_3RealPac.jpg'
+import img_4 from '../Assets/img_4newyork-smokingman.jpg'
+import img_5 from '../Assets/img_5Dystopian-light-colored-man.jpg'
+import img_6 from '../Assets/img_6Superman-flying.jpg'
+import img_7 from '../Assets/img_7Metro-vibe.jpg'
+import img_8 from '../Assets/img_8Dystopian-dark-colored-man.jpg'
+import img_9 from '../Assets/img_9Skyscraper.jpg'
+import img_10 from '../Assets/img_10futureistic-city.jpg'
+import img_11 from '../Assets/img_11dreads-blackman-portrait.jpg'
+import img_12 from '../Assets/img_12Police.jpg'
+import img_13 from '../Assets/img_13Dystopian police.jpg'
+import img_14 from '../Assets/img_14Violentcops.jpg'
+import img_15 from '../Assets/img_15DaPopo.jpg'
+import img_16 from '../Assets/img_16Big-Man.jpg'
+import img_17 from '../Assets/img_17Astronout-standing.jpg'
+import img_18 from '../Assets/img_18doggo-bear2.jpg'
+import img_19 from '../Assets/img_19Astronout-floating.jpg'
+import img_20 from '../Assets/img_20nature-astronout.jpg'
+import img_21 from '../Assets/img_21nature-astronout2.jpg'
+import img_22 from '../Assets/img_22starcity.jpg'
+import img_23 from '../Assets/img_23ronald regan bu 0.png'
+import img_24 from '../Assets/img_24samuraiman.jpg'
+import img_25 from '../Assets/img_25MercedesDopeCar.jpg'
+import img_26 from '../Assets/img_26AI Mercedes.jpg'
+import img_27 from '../Assets/img_27dope-bulletTrain.jpg'
+import img_28 from '../Assets/img_28paris-onfire.jpg'
+import img_29 from '../Assets/img_29back in thedaycar.jpg'
+import img_30 from '../Assets/img_30Black-white-astronout.jpg'
+
+
+
+let all_product = [
+  {
+    id: 1,
+    name: 'Big colored city',
+    image: img_1,
+    new_price:400.00,
+    old_price:800.00,
+  },
+  {
+    id: 2,
+    name: 'A black and white city',
+    image: img_2,
+    new_price:500.00,
+    old_price:800.00,
+  },
+  {
+    id: 3,
+    name: 'Police Brutality 2 pac',
+    image: img_3,
+    new_price:550.00,
+    old_price:800.00,
+  },
+  {
+    id: 4,
+    name: 'cigarette addict',
+    image: img_4,
+    new_price:650.00,
+    old_price:900.00,
+  },
+  {
+    id: 5,
+    name: 'Dystopian light colored man',
+    image: img_5,
+    new_price:400.00,
+    old_price:800.00,
+  },
+  {
+    id: 6,
+    name: 'Super man being a bro',
+    image: img_6,
+    new_price:500.00,
+    old_price:800.00,
+  },
+  {
+    id: 7,
+    name: 'Metro vibe',
+    image: img_7,
+    new_price:550.00,
+    old_price:800.00,
+  },
+  {
+    id: 8,
+    name: 'Dystopian dark colored man',
+    image: img_8,
+    new_price:650.00,
+    old_price:900.00,
+  },
+  {
+    id: 9,
+    name: 'Realistic skyscraper',
+    image: img_9,
+    new_price:400.00,
+    old_price:800.00,
+  },
+  {
+    id: 10,
+    name: 'Future City',
+    image: img_10,
+    new_price:500.00,
+    old_price:800.00,
+  },
+  {
+    id: 11,
+    name: 'Metro vibe',
+    image: img_11,
+    new_price:550.00,
+    old_price:800.00,
+  },
+  {
+    id: 12,
+    name: 'Popo at it again',
+    image: img_12,
+    new_price:650.00,
+    old_price:900.00,
+  },
+  {
+    id: 13,
+    name: 'Dystopian dark colored man',
+    image: img_13,
+    new_price:650.00,
+    old_price:900.00,
+  },
+  {
+    id: 14,
+    name: 'Realistic skyscraper',
+    image: img_14,
+    new_price:400.00,
+    old_price:800.00,
+  },
+  {
+    id: 15,
+    name: 'Future City',
+    image: img_15,
+    new_price:500.00,
+    old_price:800.00,
+  },
+  {
+    id: 16,
+    name: 'Big colored city',
+    image: img_16,
+    new_price:400.00,
+    old_price:800.00,
+  },
+  {
+    id: 17,
+    name: 'A black and white city',
+    image: img_17,
+    new_price:500.00,
+    old_price:800.00,
+  },
+  {
+    id: 18,
+    name: 'Police Brutality 2 pac',
+    image: img_18,
+    new_price:550.00,
+    old_price:800.00,
+  },
+  {
+    id: 19,
+    name: 'cigarette addict',
+    image: img_19,
+    new_price:650.00,
+    old_price:900.00,
+  },
+  {
+    id: 20,
+    name: 'Dystopian light colored man',
+    image: img_20,
+    new_price:400.00,
+    old_price:800.00,
+  },
+  {
+    id: 21,
+    name: 'Super man being a bro',
+    image: img_21,
+    new_price:500.00,
+    old_price:800.00,
+  },
+  {
+    id: 22,
+    name: 'Metro vibe',
+    image: img_22,
+    new_price:550.00,
+    old_price:800.00,
+  },
+  {
+    id: 23,
+    name: 'Dystopian dark colored man',
+    image: img_23,
+    new_price:650.00,
+    old_price:900.00,
+  },
+  {
+    id: 24,
+    name: 'Realistic skyscraper',
+    image: img_24,
+    new_price:400.00,
+    old_price:800.00,
+  },
+  {
+    id: 25,
+    name: 'Future City',
+    image: img_25,
+    new_price:500.00,
+    old_price:800.00,
+  },
+  {
+    id: 26,
+    name: 'Metro vibe',
+    image: img_26,
+    new_price:550.00,
+    old_price:800.00,
+  },
+  {
+    id: 27,
+    name: 'Popo at it again',
+    image: img_27,
+    new_price:650.00,
+    old_price:900.00,
+  },
+  {
+    id: 28,
+    name: 'Dystopian dark colored man',
+    image: img_28,
+    new_price:650.00,
+    old_price:900.00,
+  },
+  {
+    id: 29,
+    name: 'Realistic skyscraper',
+    image: img_29,
+    new_price:400.00,
+    old_price:800.00,
+  },
+  {
+    id: 30,
+    name: 'Future City',
+    image: img_30,
+    new_price:500.00,
+    old_price:800.00,
+  },
+  // Add more image objects as needed
+];
+
+export default all_product;
