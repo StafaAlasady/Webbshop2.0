8586d808bf2041bc3ef7b97bc5b7008baf3d03a6
StafaAlasady <Mustafa.alasady@outlook.com>
2024-03-10 22:01:26 +0100

    fixat på egen product sidan

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descritionbox-navigator">
+          <div className="descriptionbox-nav-box">Description</div>
+          <div className="descriptionbox-nav-box">Reviews (122)</div>
+        </div>
+    </div>
+  )
+}
